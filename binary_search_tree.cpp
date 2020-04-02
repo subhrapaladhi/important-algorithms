69d715e7505ba26261c31578fb9ec1ccaaa9b05e
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-04-02 20:48:00 +0530

    delete operation

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -14,7 +14,9 @@ class node{
         }
 
         node* insert(node*, int);
-        void search(node*, int);
+        node* search(node*, int);
+        node* deleteNode(node*, int);
+        node* minNode(node*);
         void inorderTraversal(node*);
 };
 
@@ -30,8 +32,13 @@ int main(){
 
     // bst.inorderTraversal(root);
     int searchTerm;
+    cout<<"enter search term: ";
     cin>>searchTerm;
-    bst.search(root, searchTerm);
+    bst.inorderTraversal(root);
+    cout<<endl;
+    node *pos = bst.deleteNode(root, searchTerm);
+    bst.inorderTraversal(pos);
+    // node *pos = bst.search(root, searchTerm);
     return 0;
 }
 
@@ -58,13 +65,53 @@ void node::inorderTraversal(node *root){
     inorderTraversal(root->right);
 }
 
-void node::search(node *root, int searchTerm){
-    if(root == NULL)
-        cout<<"not found"<<endl;
-    else if(root->data == searchTerm)
-        cout<<"found"<<endl;
+node* node::search(node *root, int searchTerm){
+    if(root==NULL or root->data == searchTerm)
+        return root;
     else if(root->data < searchTerm)
         search(root->right, searchTerm);
     else if(root->data > searchTerm)
         search(root->left, searchTerm);
+}
+
+node* node::deleteNode(node *root, int searchTerm){
+    if(root==NULL)
+        return root;
+    else if(root->data < searchTerm)
+        root->right = deleteNode(root->right, searchTerm);
+    else if(root->data > searchTerm)
+        root->left = deleteNode(root->left, searchTerm);
+    else{
+        if(root->left == NULL){
+            node *temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        else if(root->right == NULL){
+            node *temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        else{
+            node *succParent = root;
+            node *succ = root->right;
+            while(succ->left!=NULL){
+                succParent = succ;
+                succ = succ->left;
+            }
+
+            // for the first time succ->left != NULL therefore succParent!=root;
+            if(succParent != root)
+                succParent->left = succ->right;
+            // for the first time succ->left == NULL therefore succParent==root;
+            else
+                succParent->right = succ->right;
+            root->data = succ->data;
+            delete succ;
+            return root;
+        }
+    }
+    return root;
 }
