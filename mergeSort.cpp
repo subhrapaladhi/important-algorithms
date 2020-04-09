6e24535619102815e8446564ad65d28b98052de7
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-04-09 20:35:06 +0530

    changing name to merge sort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -5,8 +5,8 @@ void mergeSort(int*,int, int, int*);
 void merge(int*, int, int, int, int*);
 
 int main(){
-    int n = 8;
-    int a[n] = {0,2,7,2,4,1,9,0}, b[n];
+    int n = 9;
+    int a[n] = {7,0,2,7,2,4,1,9,0}, b[n];
 
     mergeSort(a, 0, n-1, b);
   
