bea4072e74500f79736c7f2092204f6694235f18
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-04-19 22:13:45 +0530

    a lot

--- Trees/level-order-traversal.cpp
+++ Trees/level-order-traversal.cpp
@@ -1,3 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left, *right;
+    
+        Node(int data = 0){
+            this->data = data;
+            this->left = nullptr;
+            this->right = nullptr;
+        }
+};
+
+Node* addNode(Node*, int);
+void inOrderTraversal(Node*);
+
 void levelOrderSearch(Node *root){
     if(root==nullptr)
         return;
@@ -7,10 +25,54 @@ void levelOrderSearch(Node *root){
     while(!q.empty()){
         Node *current = q.front();
         q.pop();
-        cout<<current->data;
+        cout<<current->data<<endl;
         if(current->left != nullptr)
             q.push(current->left);
         if(current->right != nullptr)
                 q.push(current->right);
     }
+}
+
+int main(){
+    Node *root = nullptr;
+    root = addNode(root, 5);
+    addNode(root, 3);
+    addNode(root, 2);
+    addNode(root, 1);
+    addNode(root, 4);
+    addNode(root, 6);
+    addNode(root, 8);
+    addNode(root, 7);
+    addNode(root, 9);
+
+    inOrderTraversal(root);
+    cout<<endl<<endl;
+    levelOrderSearch(root);
+    return 0;
+}
+
+Node* addNode(Node *root, int data){
+    Node *newNode = new Node(data), *p = root;
+
+    if(root == nullptr)
+        return newNode;
+    
+    if(data<=root->data){
+        root->left = addNode(root->left, data);
+    }
+    else{
+        root->right = addNode(root->right, data);
+    }
+
+    return root;
+}
+
+
+void inOrderTraversal(Node *root){
+    if(root == nullptr)
+        return ;
+    
+    inOrderTraversal(root->left);
+    cout<<root->data<<endl;
+    inOrderTraversal(root->right);
 }
