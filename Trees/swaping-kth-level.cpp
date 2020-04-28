620819d164119d0bc246bc5d5e5522f27235cbea
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-04-28 19:27:18 +0530

    swap kth level

--- Trees/swaping-kth-level.cpp
+++ Trees/swaping-kth-level.cpp
@@ -13,22 +13,94 @@ class Node{
         }       
 };
 
-int main(){
+void inorder(Node*);
+void swapSubTreesOfLevel(Node*, int);
 
+int main(){
     int n;
     cin>>n;
-    queue<Node*> q;
     
-    auto newNode = [](int data){return new Node(data);};
-
-    int temp1, temp2;
-
-    Node* root = newNode(1);
+    Node *root = new Node(1);
+    
+    queue<Node*> q;
     q.push(root);
-    for(int i=0; i<n; i++){
-        cin>>temp1>>temp2;
-        
+    q.push(nullptr);
+    
+    int level = 1, a, b;
+    
+    while(n>0 && !q.empty()){
+        Node *temp = q.front();
+        q.pop();
+
+        if(temp==nullptr){
+            if(!q.empty())
+                q.push(nullptr);
+            level++;
+        } else {
+            cin>>a>>b;
+            if(a!=-1){
+                temp->left = new Node(a);
+                q.push(temp->left);
+            }
+            if(b!=-1){
+                temp->right = new Node(b);
+                q.push(temp->right);
+            }
+            n--;
+        }
+    }
+
+    int t, k, itr, lvl;
+    cin>>t;
+    for(int i=0; i<t; ++i){
+        cin>>k;
+        itr = 2;
+        lvl = k;
+        while(lvl<=level){
+            swapSubTreesOfLevel(root, lvl);
+            lvl = itr*k;
+            itr++;
+        }
+        inorder(root);
+        cout<<endl;
     }
 
     return 0;
+}
+
+void swapSubTreesOfLevel(Node *root, int k){
+    queue<Node*> q;
+    q.push(root);
+    q.push(nullptr);
+    int level = 1;
+
+    while(!q.empty()){
+        Node *temp = q.front();
+        q.pop();
+
+        if(temp==nullptr){
+            if(!q.empty())
+                q.push(nullptr);
+            level++;
+        } else {
+            if(level == k){
+                Node *sw = temp->left;
+                temp->left = temp->right;
+                temp->right = sw;
+            }
+            if(temp->left)
+                q.push(temp->left);
+            if(temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+void inorder(Node *root){
+    if(root==nullptr)
+        return ;
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
 }
