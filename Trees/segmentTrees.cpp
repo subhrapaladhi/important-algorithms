83a25df6124efbfffff8fa316e9cde9c57f405d7
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-06-23 08:42:10 +0530

    get sum for a range

--- Trees/segmentTrees.cpp
+++ Trees/segmentTrees.cpp
@@ -14,6 +14,20 @@ void buildSegmentTree(vector<int>& a, vector<int>& st, int start, int end, int n
     st[node] = st[2*node+1] + st[2*node+2];
 }
 
+int getSum(vector<int>& st, int start, int end, int rstart, int rend, int node){
+    if(rstart<=start && rend>=end){
+        return st[node];
+    }
+
+    if(rstart>end || rend<start){
+        return 0;
+    }
+
+    int mid = (start+end)/2;
+
+    return getSum(st, start, mid, rstart, rend, 2*node+1) + getSum(st, mid+1, end, rstart, rend, 2*node+2);
+}
+
 int main(){
     int n=10;
     vector<int> a{1,2,3,4,5,6,7,8,9,10};
@@ -25,8 +39,10 @@ int main(){
     buildSegmentTree(a,st,0,n-1,0);
     
     for(int i=0; i<len; i++){
-        cout<< i<<" : "<<st[i]<<endl;
+        cout<<st[i]<<"  ";
     }
     cout<<"\n\n";
+
+    cout<<"sum = "<<getSum(st, 0, n-1, 7, 9, 0)<<endl;
     return 0;
 }
