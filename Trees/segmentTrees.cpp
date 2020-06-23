ced2aae9e23e459d242d0ed3e607489b0a370137
subhrapaladhi <subhrapaladhi9@gmail.com>
2020-06-23 10:32:36 +0530

    refactoring code

--- Trees/segmentTrees.cpp
+++ Trees/segmentTrees.cpp
@@ -1,40 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// CREATE
+vector<int> createSegmentTree(vector<int>&,int);
 void buildSegmentTree(vector<int>&, vector<int>&, int, int, int);
-int getSum(vector<int>&, int , int, int, int, int);
+
+// GET SUM
+int getSum(vector<int>&, int, int, int);
+int getSumRange(vector<int>&, int , int, int, int, int);
+
+// UPDATE
 void update(vector<int>&, vector<int>&, int, int, int);
 void updateValue(vector<int>&, int, int, int, int, int);
+
+// PRINT
+void printTree(vector<int>, int);
 
 int main(){
     int n=10;
     vector<int> a{1,2,3,4,5,6,7,8,9,10};
     
-    int h = floor(log2(2*n-1))+1;
-    int len = pow(2,h)-1;
-    vector<int> st(len, -1);
-
-    buildSegmentTree(a,st,0,n-1,0);
+    vector<int> st = createSegmentTree(a,n);
     
-    for(int i=0; i<len; i++){
-        cout<<st[i]<<"  ";
-    }
-    cout<<"\n\n";
+    printTree(st,n);
 
-    cout<<"sum = "<<getSum(st, 0, n-1, 7, 9, 0)<<endl;
+    cout<<"sum = "<<getSum(st, n, 7, 9)<<endl;
 
-    int value = 15;
-    update(a, st, value, 4, n);
+    update(a, st, 15, 4, n);
     
-    cout<<"\nupdated\n\n";
-    for(int i=0; i<len; i++){
-        cout<<st[i]<<"  ";
-    }
-    cout<<"\n\n";
+    cout<<"updated\n";
+    printTree(st,n);
 
     return 0;
 }
 
+
+
+// CREATE SEGEMENT TREE
+
+vector<int> createSegmentTree(vector<int>& a, int n){
+    int h = floor(log2(2*n-1))+1;
+    int len = pow(2,h)-1;
+    vector<int> st(len, -1);
+
+    buildSegmentTree(a, st, 0, n-1, 0);
+
+    return st;
+}
 
 void buildSegmentTree(vector<int>& a, vector<int>& st, int start, int end, int node){
     if(start==end){
@@ -50,7 +62,18 @@ void buildSegmentTree(vector<int>& a, vector<int>& st, int start, int end, int n
 }
 
 
-int getSum(vector<int>& st, int start, int end, int rstart, int rend, int node){
+
+// GET SUM FOR A RANGE
+
+int getSum(vector<int>& st, int n, int rstart, int rend){
+    if(rstart>rend || rstart<0 || rend>n-1){
+        return -1;
+    }
+
+    return getSumRange(st,0,n-1,rstart, rend, 0);
+}
+
+int getSumRange(vector<int>& st, int start, int end, int rstart, int rend, int node){
     if(rstart<=start && rend>=end){
         return st[node];
     }
@@ -61,8 +84,12 @@ int getSum(vector<int>& st, int start, int end, int rstart, int rend, int node){
 
     int mid = (start+end)/2;
 
-    return getSum(st, start, mid, rstart, rend, 2*node+1) + getSum(st, mid+1, end, rstart, rend, 2*node+2);
+    return getSumRange(st, start, mid, rstart, rend, 2*node+1) + getSumRange(st, mid+1, end, rstart, rend, 2*node+2);
 }
+
+
+
+// UPDATE A VALUE IN THE ORIGINAL ARRAY AND SEGMENT TREE
 
 void update(vector<int>& a, vector<int>& st, int value, int index, int n){
     int diff = value - a[index];
@@ -83,4 +110,18 @@ void updateValue(vector<int>& st, int diff, int index, int start, int end, int n
         updateValue(st, diff, index, start, mid, 2*node+1);
         updateValue(st, diff, index, mid+1, end, 2*node+2);
     }
+}
+
+
+
+// PRINT SEGMENT TREE
+
+void printTree(vector<int> st, int n){
+    int h = floor(log2(2*n-1))+1;
+    int len = pow(2,h)-1;
+
+    for(int i=0; i<len; i++){
+        cout<<st[i]<<"  ";
+    }
+    cout<<endl;
 }
