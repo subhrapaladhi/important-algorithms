821b5da1d54baff6f48918fb961bd7c0c8ea4cd5
subhrapaladhi <subhrapaladhi9@gmail.com>
2021-03-02 12:04:53 +0530

    min cost climbing stairs

---
+++ Dynamic Programming/Min Cost Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCostClimbingStairs(vector<int> cost){
+    int n = cost.size();
+    vector<int> a(n);
+    a[0] = cost[0], a[1] = cost[1];
+
+    for(int i=2; i<n; i++){
+        a[i] = cost[i] + min(a[i-1],a[i-2]);
+    }
+
+    return min(a[n-1],a[n-2]);
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout<<minCostClimbingStairs({1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
+
+    return 0;
+}
