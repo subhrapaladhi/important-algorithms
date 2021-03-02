31c1d70c90543e6622bb283fe0d6bc818dc3618d
subhrapaladhi <subhrapaladhi9@gmail.com>
2021-03-02 11:32:03 +0530

    climbing stairs

---
+++ Dynamic Programming/Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countWays(int n){
+    vector<int> a(n+1);
+    a[0]=1, a[1]=1;
+    
+    for(int i=2; i<=n; i++){
+        a[i] = a[i-1] + a[i-2];
+    }
+
+    return a[n];
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n=10;
+    cout<<countWays(n);
+    
+    return 0;
+}
